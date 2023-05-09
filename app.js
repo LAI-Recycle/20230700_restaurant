f7b2581f2f678a68e1407b1fa2b1893ff31ea7fb
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-09 14:21:30 +0800

    feat: add hbs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
 
 if (process.env.NODE_ENV !== 'production') {
     require("dotenv").config()
@@ -19,6 +21,18 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('MongoDB connected!')
 })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    Restaurant.find()
+      .lean()
+      .then((restaurants) => res.render('index', { restaurants }))
+      .catch((error) => console.error(error))
+  })
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}` )
