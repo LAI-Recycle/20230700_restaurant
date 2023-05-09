991cc9e5b3c4017dd635557167170a48deca68ad
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-09 23:07:57 +0800

    feat: add delete restaurant function

--- models/restaurant.js
+++ models/restaurant.js
@@ -8,27 +8,21 @@ const restaurantSchema = new Schema({
   },
   name_en: {
     type: String,
-    required: true,
   },
   category: {
     type: String,
-    required: true,
   },
   image: {
     type: String,
-    required: true,
   },
   location: {
     type: String,
-    required: true,
   },
   phone: {
     type: String,
-    required: true,
   },
   google_map: {
     type: String,
-    required: true,
   },
   rating: {
     type: Number,
@@ -36,7 +30,6 @@ const restaurantSchema = new Schema({
   },
   description: {
     type: String,
-    required: true,
   },
 })
 
