3bb952e670a1aec350583a5a56be18facc440d88
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-09 12:31:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const port = 3000
+const mongoose = require('mongoose')
 
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config()
+  }
+
+//set MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('MongoDB error')
 })
 
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+db.once('open', () => {
+  console.log('MongoDB connected!')
 })
+
+app.listen(port, () => {
+    console.log(`Express is listening on localhost:${port}` )
+  })
