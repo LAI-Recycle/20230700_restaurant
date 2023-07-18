179b9e3df750817711624a97ed1b737f601402ac
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-07-19 00:55:23 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,10 @@ const home = require('./modules/home')
 const users = require('./modules/users') 
 const { model } = require('mongoose')
 const restaurants = require('./modules/restaurants')
+const { authenticator } = require('../middleware/auth') 
 
-
-
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
-router.use('/restaurants', restaurants)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
