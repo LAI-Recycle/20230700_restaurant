a873abdf1f967ca3f9d7cb70f18f1ec1fdea0d1c
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-07-19 21:17:14 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 module.exports = app => {
@@ -12,12 +13,14 @@ module.exports = app => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered!' })
         }
-        if (user.password !== password) {
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
           return done(null, false, { message: 'Email or Password incorrect.' })
         }
         return done(null, user)
       })
-      .catch(err => done(err, false))
+    })
+    .catch(err => done(err, false))
   }))
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
